3cd6775578089f5001176e27a961ef058354e9ae
Michael Dvorkin <mike@dvorkin.net>
2014-11-29 14:28:10 -0800

    Ante implementation in Rust

---
+++ ante.rs
@@ -0,0 +1,59 @@
+// Copyright (c) 2013-2104 Michael Dvorkin
+// Ante is an esoteric programming language where all you've got is a deck of cards.
+//
+// This is Ante implementation in Rust.
+
+extern crate regex;
+extern crate num;
+
+use std::io::File;
+use std::collections::HashMap;
+
+use regex::Regex;
+use num::bigint::BigInt;
+
+
+struct Card {
+	rank: u32,
+	suit: u32
+}
+
+struct Ante {
+	pc:     int,                    // Program counter (index within ante.code)
+	line:   int,                    // Current line number.
+    code:   Vec<Card>,              // Array of cards.
+    vars:   HashMap<char, uint>,    // Four registers hashed by suit.
+    labels: HashMap<uint, uint>,    // Labels for ante.pc to jump to.
+    buffer: Vec<char>               // Buffer to collect UTF-8 character bytes.
+}
+
+impl Ante {
+    fn new() -> Ante {
+        let mut vars = HashMap::new();
+        vars.insert('♦', 0);
+        vars.insert('♥', 0);
+        vars.insert('♠', 0);
+        vars.insert('♣', 0);
+
+        Ante {
+            pc:     0,
+            line:   0,
+            code:   vec![],
+            vars:   vars,
+            labels: HashMap::new(),
+            buffer: vec![]
+        }
+    }
+
+    fn run(& mut self, filename: &str) {
+        let mut file = File::open(&Path::new(filename));
+        let program = file.read_to_string().unwrap();
+        println!("file: {}", program);
+    }
+}
+
+
+fn main() {
+    println!("usage: ante filename.ante");
+    Ante::new().run("hello.ante".as_slice());
+}
