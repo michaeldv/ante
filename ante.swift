265857a0036c94726c502c48cdce63f7b083889d
Michael Dvorkin <mike@dvorkin.net>
2014-12-15 21:19:29 -0800

    Ante implementation in Swift

---
+++ ante.swift
@@ -0,0 +1,8 @@
+#!/usr/bin/env xcrun swift
+//
+// Copyright (c) 2014 Michael Dvorkin
+// Ante is an esoteric programming language where all you've got is a deck of cards.
+//
+// This is Ante implementation in Swift.
+//
+println("Hello Ante!")
